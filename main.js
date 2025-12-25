43feb1cf275afabd40fe0de8bfa3adaabe82dbbc
aziztraorer <aziztraore55801@gmail.com>
2025-12-25 12:33:14 +0100

    ajout mon  portfolio

---
+++ main.js
@@ -0,0 +1,5 @@
+const menubtn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+menubtn.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+});
